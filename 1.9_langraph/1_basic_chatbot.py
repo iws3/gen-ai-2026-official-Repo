a6c2353c29478aa0d4355bfe07b104846ff4af20
AI Course Developer <iwstechnical7@gmail.com>
2026-07-19 13:04:51 +0100

    exploring profiles in models

---
+++ 1.9_langraph/1_basic_chatbot.py
@@ -0,0 +1,14 @@
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+
+# add_messages is called reducers
+
+class State(TypedDict):
+    messages:Annotated[list, add_messages]
+    
+
+graph_builder=StateGraph(State)
+
+print(graph_builder)
